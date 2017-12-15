15044cb9c4a6a0175e3363f2435bec525078a98f
Oleksandr Halahan <Oleksandr.Halahan@portaone.com>
2017-12-15 07:44:33 +0200

    Implement interruptions

--- esp_json/esp_json.ino
+++ esp_json/esp_json.ino
@@ -8,12 +8,33 @@
 #include <ArduinoJson.h>
 
 ESP8266WebServer server;
-uint8_t pin_led = 16;
-char* ssid = "*****";
-char* password = "******";
+
+#define firstLine 5
+#define secondLine 4
+
+#define zeroPin 2
+
+int firstDelay = 20;
+int secondDelay = 40;
+
+int dimming = 128;
+
+char* ssid = "****";
+char* password = "****";
+
+int counter = 0;
 
 void setup()
 {
+  analogWriteFreq(100);
+  pinMode(firstLine, OUTPUT);
+  digitalWrite(firstLine, LOW);
+  pinMode(secondLine, OUTPUT);
+  digitalWrite(secondLine, LOW);
+
+  pinMode(zeroPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(zeroPin), detectChange, RISING);
+
   WiFi.begin(ssid, password);
   Serial.begin(115200);
   while (WiFi.status() != WL_CONNECTED)
@@ -34,11 +55,26 @@ void setup()
 void loop()
 {
   server.handleClient();
+  for (int i = 5; i <= 128; i++) {
+    dimming = i;
+    delay(10);
+  }
+}
+
+void detectChange() {
+  int dimtime = (65 * dimming);  // For 60Hz =>65
+  delayMicroseconds(dimtime);    // Off cycle
+  digitalWrite(firstLine, HIGH);   // triac firing
+  digitalWrite(secondLine, HIGH);   // triac firing
+  delayMicroseconds(8.33);         // triac On propagation delay (for 60Hz use 8.33)
+  digitalWrite(firstLine, LOW);   // triac firing
+  digitalWrite(secondLine, LOW);   // triac firing
 }
 
 void getInfo() {
   String json = "{";
-  json += "led: true";
+  json += "counter: " + String(counter);
+  json += "\n";
   json += "}";
   server.send(200, "text/json", json);
 }
